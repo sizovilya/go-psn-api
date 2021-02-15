c1299db6f4da2fabf45ce3dc395d4ed9acfae202
Ilya Sizov <sizov.ilya@gmail.com>
2021-02-16 00:19:15 +0300

    Base client (#3)
    * auth func
    
    * Update client.go
    
    * refresh tokens method
    
    * lint

--- client.go
+++ client.go
@@ -1,12 +1,45 @@
-package go_psn_api
+package psn
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 type psn struct {
-
+	http *http.Client
+	lang string
+	region string
+	npsso string
+	refreshToken string
+	accessToken string
 }
 
-func NewPsnApi () *psn {
-	fmt.Println("hello world")
-	return &psn{}
+func NewPsnApi (lang, region, npsso, refreshToken, accessToken string) *psn {
+	return &psn{
+		http:         &http.Client{},
+		lang:         lang,
+		region:       region,
+		npsso:        npsso,
+		refreshToken: refreshToken,
+		accessToken:  accessToken,
+	}
+}
+
+func (p *psn) Auth() error {
+	tokens, err :=p.authRequest()
+	if err != nil {
+		return fmt.Errorf("Can't do auth request %w: ", err)
+	}
+	p.accessToken = tokens.AccessToken
+	p.refreshToken = tokens.RefreshToken
+	return nil
+}
+
+func (p *psn) RefreshTokens() error {
+	tokens, err :=p.refreshTokens()
+	if err != nil {
+		return fmt.Errorf("Can't refresh tokens %w: ", err)
+	}
+	fmt.Println(tokens)
+	return nil
 }
