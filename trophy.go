a702fa1cccfd0c9e066f2d1180c8b4db8d529142
Ilya Sizov <sizov.ilya@gmail.com>
2021-03-01 23:05:40 +0300

    Update trophy.go

--- trophy.go
+++ trophy.go
@@ -13,24 +13,24 @@ func (p *psn) GetTrophyTitles(name string) (profile *Profile, err error) {
 	h["Accept-Encoding"] = "gzip, deflate, br"
 
 	userResponse := &ProfileResponse{}
-	//err = p.get(
-	//	fmt.Sprintf(
-	//		"https://%s-tpy.np.community.playstation.net/trophy/v1/trophyTitles?fields=@default&npLanguage=%s&iconSize=m&platform=PS3,PSVITA,PS4&offset=0&limit=20&comparedUser=%s",
-	//		//"https://%s%sfields=@default,trophyTitleSmallIconUrl&platform=PS3,PS4,PSVITA&limit=12&offset=0&comparedUser=%s&npLanguage=ru",
-	//		p.region,
-	//		p.lang,
-	//		//trophyTitleApi,
-	//		name,
-	//	),
-	//	h,
-	//	&userResponse,
-	//)
-	// check scope
 	err = p.get(
-		"https://ru-tpy.np.community.playstation.net/trophy/v1/trophyTitles?fields=%40default%2CtrophyTitleSmallIconUrl&platform=PS3%2CPS4%2CPSVITA&limit=12&offset=0&npLanguage=ru",
+		fmt.Sprintf(
+			"https://%s-tpy.np.community.playstation.net/trophy/v1/trophyTitles?fields=@default&npLanguage=%s&iconSize=m&platform=PS3,PSVITA,PS4&offset=0&limit=20&comparedUser=%s",
+			//"https://%s%sfields=@default,trophyTitleSmallIconUrl&platform=PS3,PS4,PSVITA&limit=12&offset=0&comparedUser=%s&npLanguage=ru",
+			p.region,
+			p.lang,
+			//trophyTitleApi,
+			name,
+		),
 		h,
 		&userResponse,
 	)
+	// check scope
+	//err = p.get(
+	//	"https://ru-tpy.np.community.playstation.net/trophy/v1/trophyTitles?fields=%40default%2CtrophyTitleSmallIconUrl&platform=PS3%2CPS4%2CPSVITA&limit=12&offset=0&npLanguage=ru",
+	//	h,
+	//	&userResponse,
+	//)
 	if err != nil {
 		return nil, fmt.Errorf("can't do GET request: %w", err)
 	}
