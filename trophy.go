f56335f0ff1e365d5c9e474b17cee42bf76b123e
Ilya Sizov <sizov.ilya@gmail.com>
2021-03-02 08:48:18 +0300

    Update trophy.go

--- trophy.go
+++ trophy.go
@@ -15,8 +15,7 @@ func (p *psn) GetTrophyTitles(name string) (profile *Profile, err error) {
 	userResponse := &ProfileResponse{}
 	err = p.get(
 		fmt.Sprintf(
-			"https://%s-tpy.np.community.playstation.net/trophy/v1/trophyTitles?fields=@default&npLanguage=%s&iconSize=m&platform=PS3,PSVITA,PS4&offset=0&limit=20&comparedUser=%s",
-			//"https://%s%sfields=@default,trophyTitleSmallIconUrl&platform=PS3,PS4,PSVITA&limit=12&offset=0&comparedUser=%s&npLanguage=ru",
+			"https://%s%sfields=@default,trophyTitleSmallIconUrl&platform=PS3,PS4,PSVITA&limit=12&offset=0&comparedUser=%s&npLanguage=ru",
 			p.region,
 			p.lang,
 			//trophyTitleApi,
